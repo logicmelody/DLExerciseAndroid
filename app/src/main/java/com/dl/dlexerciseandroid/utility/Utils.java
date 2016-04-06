20e6f00fd23afbddd87e0a7d4ffc6010e3966b27
logicmelody <logicmelody@gmail.com>
2016-04-06 16:27:27 +0800

    [Theme] Change theme color

--- app/src/main/java/com/dl/dlexerciseandroid/utility/Utils.java
+++ app/src/main/java/com/dl/dlexerciseandroid/utility/Utils.java
@@ -1,5 +1,8 @@
 package com.dl.dlexerciseandroid.utility;
 
+import android.app.Activity;
+import android.view.WindowManager;
+
 /**
  * Created by logicmelody on 2016/3/30.
  */
@@ -12,5 +15,14 @@ public class Utils {
         public static final String OVERVIEW = "com.dl.dlexerciseandroid.OVERVIEW_FRAGMENT";
         public static final String CONSUMING_RESTFUL_WEB_SERVICE = "com.dl.dlexerciseandroid.CONSUMING_RESTFUL_WEB_SERVICE_FRAGMENT";
         public static final String TEST = "com.dl.dlexerciseandroid.TEST_FRAGMENT";
+    }
+
+    public static void setStatusBarTranslucent(Activity activity, boolean makeTranslucent) {
+        if (makeTranslucent) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        } else {
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 }
