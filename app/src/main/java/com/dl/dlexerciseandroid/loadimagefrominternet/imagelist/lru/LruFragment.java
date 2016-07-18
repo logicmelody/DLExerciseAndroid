6ce21f11e8640cf656932270ed237d7edea5e44c
logicmelody <logicmelody@gmail.com>
2016-07-18 22:08:07 +0800

    [LoadImageFromInternet] Restore LruCache after rotation

--- app/src/main/java/com/dl/dlexerciseandroid/loadimagefrominternet/imagelist/lru/LruFragment.java
+++ app/src/main/java/com/dl/dlexerciseandroid/loadimagefrominternet/imagelist/lru/LruFragment.java
@@ -2,12 +2,16 @@ package com.dl.dlexerciseandroid.loadimagefrominternet.imagelist.lru;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.util.LruCache;
 import android.util.Log;
 
 import com.dl.dlexerciseandroid.loadimagefrominternet.imagelist.base.ImageListAdapter;
 import com.dl.dlexerciseandroid.loadimagefrominternet.imagelist.base.ImageListFragment;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -17,16 +21,23 @@ public class LruFragment extends ImageListFragment {
 
     public static final String TAG = LruFragment.class.getName();
 
+    public static final String EXTRA_MEMORY_CACHE = "com.dl.dlexerciseandroid.EXTRA_MEMORY_CACHE";
+
     private LruCache<String, Bitmap> mMemoryCache;
 
 
     @Override
-    protected ImageListAdapter getImageListAdapter(Context context, List<String> imageDataList) {
-        setupMemoryCache();
-        return new LruAdapter(context, imageDataList, mMemoryCache);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        setupMemoryCache(savedInstanceState);
+        super.onActivityCreated(savedInstanceState);
     }
 
-    private void setupMemoryCache() {
+    private void setupMemoryCache(Bundle savedInstanceState) {
+        createMemoryCache();
+        restoreMemoryCache(savedInstanceState);
+    }
+
+    private void createMemoryCache() {
         // Get max available VM memory, exceeding this amount will throw an
         // OutOfMemory exception. Stored in kilobytes as LruCache takes an
         // int in its constructor.
@@ -48,6 +59,46 @@ public class LruFragment extends ImageListFragment {
                 return bitmap.getByteCount() / 1024;
             }
         };
+    }
+
+    private void restoreMemoryCache(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            return;
+        }
+
+        HashMap<String, Bitmap> map = (HashMap<String, Bitmap>) savedInstanceState.getSerializable(EXTRA_MEMORY_CACHE);
+
+        if (map == null || map.keySet() == null) {
+            return;
+        }
+
+        Iterator<String> iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            Bitmap bitmap = map.get(key);
+
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    @Override
+    protected ImageListAdapter getImageListAdapter(Context context, List<String> imageDataList) {
+        return new LruAdapter(context, imageDataList, mMemoryCache);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveMemoryCacheState(outState);
+    }
+
+    // In onSaveInstanceState, try using LruCache.snapshot() to get a Map of your cache entries,
+    // ordered from least to most recently used. You could then write each key,value into the Bundle.
+    // Then when restoring from the Bundle, iterate each entry and put() them back into the LruCache.
+    private void saveMemoryCacheState(Bundle outState) {
+        // 因為HashMap是Serializable的object，才可以塞到bundle中傳出去
+        HashMap<String, Bitmap> map = (HashMap<String, Bitmap>) mMemoryCache.snapshot();
+        outState.putSerializable(EXTRA_MEMORY_CACHE, map);
     }
 
     @Override
