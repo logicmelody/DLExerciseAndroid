9870ad21ba55854388643835b53734b11c34fe51
logicmelody <logicmelody@gmail.com>
2016-09-27 15:23:33 +0800

    Revert "[NDK] Setup Android NDK"
    This reverts commit bf2e0abacd3d9a30d1af71f6c8a11407f0f862f7.

--- app/src/main/jni/dl-exercise-jni.c
+++
@@ -1,6 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jstring JNICALL
-Java_com_dl_dlexerciseandroid_utility_utils_NdkUtils_getMsgFromJni(JNIEnv *env, jobject instance) {
-    return (*env)->NewStringUTF(env, "Hello from Jni");
-}
