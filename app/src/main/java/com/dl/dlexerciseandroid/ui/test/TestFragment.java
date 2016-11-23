cf95320d99b000d97fc3d7ab2a1fcc2526f4bc0d
logicmelody <logicmelody@gmail.com>
2016-11-23 14:21:28 +0800

    [Test] Add log for fragment lifecycle

--- app/src/main/java/com/dl/dlexerciseandroid/ui/test/TestFragment.java
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/test/TestFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,22 +29,104 @@ public class TestFragment extends Fragment implements View.OnClickListener {
     private TextView mResultText;
 
 
+    /**
+     * 一定要實作的method!!!
+     * Called when a fragment is first attached to its context.
+     */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        Log.d("danny", "TestFragment onAttach()");
+
         mContext = context;
     }
 
+    /**
+     * 會在onAttach()之後call
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("danny", "TestFragment onCreate()");
+    }
+
+    /**
+     * 一定要實作的method!!!
+     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d("danny", "TestFragment onCreateView()");
+
         return inflater.inflate(R.layout.fragment_test, container, false);
     }
 
+    /**
+     * 一定要實作的method!!!
+     *
+     * 會在這個Fragment的Activity的onCreate()完成之後call，我們會在這個method進行find views的動作
+     *
+     *
+     * Called when the fragment's activity has been created and this fragment's view hierarchy instantiated.
+     * It can be used to do final initialization once these pieces are in place, such as retrieving views or restoring state.
+     * It is also useful for fragments that use setRetainInstance(boolean) to retain their instance,
+     * as this callback tells the fragment when it is fully associated with the new activity instance.
+     * This is called after onCreateView(LayoutInflater, ViewGroup, Bundle) and before onViewStateRestored(Bundle).
+     */
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.d("danny", "TestFragment onActivityCreated()");
+
         initialize();
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Log.d("danny", "TestFragment onViewStateRestored()");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("danny", "TestFragment onStart()");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("danny", "TestFragment onResume()");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("danny", "TestFragment onPause()");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("danny", "TestFragment onStop()");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d("danny", "TestFragment onDestroyView()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("danny", "TestFragment onDestroy()");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d("danny", "TestFragment onDetach()");
     }
 
     private void initialize() {
