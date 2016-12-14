b2f7443626f4f699d3392b536f964f05bc65bf80
logicmelody <logicmelody@gmail.com>
2016-12-14 11:53:11 +0800

    [Time] Experiment of displaying the time in US time zone and default time zone

--- app/src/main/java/com/dl/dlexerciseandroid/ui/time/TimeFragment.java
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/time/TimeFragment.java
@@ -4,17 +4,27 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dl.dlexerciseandroid.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class TimeFragment extends Fragment {
 
     public static final String TAG = TimeFragment.class.getName();
 
     private Context mContext;
+
+    private TextView mTimeMillisecond;
+    private TextView mUsTimeZoneTime;
+    private TextView mDefaultTimeZoneTime;
 
 
     @Override
@@ -37,9 +47,32 @@ public class TimeFragment extends Fragment {
 
     private void initialize() {
         findViews();
+        setupTime();
     }
 
     private void findViews() {
+        mTimeMillisecond = (TextView) getView().findViewById(R.id.text_view_time_millisecond);
+        mUsTimeZoneTime = (TextView) getView().findViewById(R.id.text_view_time_us_time_zone_time);
+        mDefaultTimeZoneTime = (TextView) getView().findViewById(R.id.text_view_time_default_time_zone_time);
+    }
 
+    private void setupTime() {
+        // Calendar拿到的物件，經由calendar.getTimeInMillis()所拿到的millisecond是UTC的標準時間，只會有一種結果
+        // Returns this Calendar's time value in milliseconds.
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormatUS = new SimpleDateFormat("EE MMM dd HH:mm:ss zzz yyyy");
+        SimpleDateFormat dateFormatDefault = new SimpleDateFormat("EE MMM dd HH:mm:ss zzz yyyy");
+
+        // 如果要根據timezone的不同來顯示對應的時間，需要在轉成text的時候進行設定
+        // 也就是對SimpleDateFormat設定timezone
+        TimeZone timeZone = TimeZone.getTimeZone("US");
+        dateFormatUS.setTimeZone(timeZone);
+
+//        Log.d("danny", "UTC 1 " + calendar.getTimeInMillis());
+//        Log.d("danny", "UTC 2 " + calendar.getTime().getTime());
+
+        mTimeMillisecond.setText(String.valueOf(calendar.getTimeInMillis()));
+        mUsTimeZoneTime.setText(dateFormatUS.format(calendar.getTime()));
+        mDefaultTimeZoneTime.setText(dateFormatDefault.format(calendar.getTime()));
     }
 }
