6adca0e02d593a37853d335cb2597ffbe5596064
logicmelody <logicmelody@gmail.com>
2016-12-22 11:24:55 +0800

    [Animation] Remove some useless code

--- app/src/main/java/com/dl/dlexerciseandroid/ui/animation/AnimationFragment.java
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/animation/AnimationFragment.java
@@ -4,22 +4,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.dl.dlexerciseandroid.R;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 /**
  * Created by logicmelody on 2016/4/8.
