54c3ced23637d40b0e18cffdf8801511d63b5eda
logicmelody <logicmelody@gmail.com>
2017-01-06 18:00:58 +0800

    [SpeechRecognition] Can detect user's voice but not finished yet

--- app/src/main/java/com/dl/dlexerciseandroid/ui/speechrecognition/SpeechRecognitionFragment.java
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/speechrecognition/SpeechRecognitionFragment.java
@@ -1,27 +1,42 @@
 package com.dl.dlexerciseandroid.ui.speechrecognition;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dl.dlexerciseandroid.R;
+
+import java.util.List;
 
 /**
  * Created by dannylin on 2017/1/6.
  */
 
-public class SpeechRecognitionFragment extends Fragment implements View.OnClickListener {
+// Reference: http://chengabriel.blogspot.tw/2016/04/android-dialog.html
+// 用 Google 提供的 SpeechRecognizer 來實作 speech to text
+public class SpeechRecognitionFragment extends Fragment implements View.OnClickListener, RecognitionListener {
 
     public static final String TAG = SpeechRecognitionFragment.class.getName();
 
     private Context mContext;
 
+    private TextView mSpeechText;
     private ImageView mMicButton;
+
+    private SpeechRecognizer mSpeechRecognizer;
+
+    private boolean mIsListening = false;
 
 
     @Override
@@ -45,9 +60,11 @@ public class SpeechRecognitionFragment extends Fragment implements View.OnClickL
     private void initialize() {
         findViews();
         setupViews();
+        setupSpeechRecognition();
     }
 
     private void findViews() {
+        mSpeechText = (TextView) getView().findViewById(R.id.text_view_speech_recognition_text);
         mMicButton = (ImageView) getView().findViewById(R.id.image_view_speech_recognition_mic_button);
     }
 
@@ -55,11 +72,164 @@ public class SpeechRecognitionFragment extends Fragment implements View.OnClickL
         mMicButton.setOnClickListener(this);
     }
 
+    private void setupSpeechRecognition() {
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(mContext);
+        mSpeechRecognizer.setRecognitionListener(this);
+    }
+
+    private Intent getRecognizerIntent() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "zh-TW");
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, mContext.getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+
+        return intent;
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.image_view_speech_recognition_mic_button:
+                if (!mIsListening) {
+                    startListening();
+
+                } else {
+                    cancel();
+                }
+
                 break;
         }
+    }
+
+    private void startListening() {
+        setMicButtonListening(true);
+        mSpeechRecognizer.startListening(getRecognizerIntent());
+    }
+
+    private void setMicButtonListening(boolean isListening) {
+        mMicButton.setImageResource(isListening ?
+                R.drawable.ic_speech_recognition_mic_recording : R.drawable.ic_speech_recognition_mic);
+
+        mIsListening = !mIsListening;
+    }
+
+    private void stopListening(){
+        mSpeechRecognizer.stopListening();
+        setMicButtonListening(false);
+    }
+
+    private void cancel(){
+        mSpeechRecognizer.cancel();
+        setMicButtonListening(false);
+    }
+
+    @Override
+    public void onDestroy() {
+        mSpeechRecognizer.destroy();
+
+        super.onDestroy();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // RecognitionListener
+    // 1. onReadyForSpeech
+    // 2. onBeginningOfSpeech
+    // 3. [trigger multiple times] onRmsChanged, onBufferReceived, onPartialResults
+    // 4. onEndOfSpeech
+    // 5. [one of them] onResult  /  onError
+    //
+    @Override
+    public void onReadyForSpeech(Bundle params) {
+        Log.d("danny", "準備就緒，可以開始說話");
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+        Log.d("danny", "檢測用戶已經開始說話");
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onError(int error) {
+        String errorMessage = getErrorText(error);
+        Log.d("danny", "FAILED " + errorMessage);
+
+        mSpeechText.setText("");
+        setMicButtonListening(false);
+    }
+
+    public String getErrorText(int errorCode) {
+        String message;
+
+        switch (errorCode) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                message = "Audio recording error";
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                message = "Client side error";
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                message = "Insufficient permissions";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                message = "Network error";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                message = "Network timeout";
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                message = "No match";
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                message = "RecognitionService busy";
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                message = "error from server";
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                message = "No speech input";
+                break;
+            default:
+                message = "Didn't understand, please try again.";
+                break;
+        }
+
+        return message;
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        setMicButtonListening(false);
+
+        // matches的ArrayList會儲存所有可能的辨識結果，通常我們會取index = 0，代表機率最高的辨識結果
+        List<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        mSpeechText.setText(matches.get(0));
+    }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+        List<String> matches = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        mSpeechText.setText(matches.get(0));
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+
     }
 }
