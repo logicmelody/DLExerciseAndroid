79c2caafe5a1ff028f2f88907feb8b284c371468
logicmelody <logicmelody@gmail.com>
2017-06-26 16:46:28 +0800

    [Testing] Add some comments

--- app/src/androidTest/java/com/dl/dlexerciseandroid/ApplicationTest.java
+++ app/src/androidTest/java/com/dl/dlexerciseandroid/ApplicationTest.java
@@ -6,6 +6,7 @@ import android.test.ApplicationTestCase;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+// 在androidTest底下的test，一定要跑在emulator或是real device上
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
