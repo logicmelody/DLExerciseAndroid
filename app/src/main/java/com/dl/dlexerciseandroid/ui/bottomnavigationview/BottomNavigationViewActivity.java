2a3caf3d1f93368f734b49ba980b8fbdc04766c8
Danny Lin <logicmelody@gmail.com>
2017-09-06 13:31:03 +0800

    Revert "[Git] Test"
    This reverts commit 1571a848699f3f0e39c7904fd5c9aa7b1de05914.

--- app/src/main/java/com/dl/dlexerciseandroid/ui/bottomnavigationview/BottomNavigationViewActivity.java
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/bottomnavigationview/BottomNavigationViewActivity.java
@@ -19,7 +19,6 @@ public class BottomNavigationViewActivity extends AppCompatActivity implements
     private BottomNavigationPresenter mBottomNavigationPresenter;
 
     private Toolbar mToolbar;
-    private int mTest = 0;
 
     private TextView mContentText;
     private BottomNavigationView mBottomBar;
