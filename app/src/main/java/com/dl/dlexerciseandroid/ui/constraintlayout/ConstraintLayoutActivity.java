f9181c71f3918501f84b6edfc033c26ba68f4a6a
Danny Lin <logicmelody@gmail.com>
2018-03-12 08:26:12 +0800

    [ConstraintLayout] ConstraintLayout practice

---
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/constraintlayout/ConstraintLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.dl.dlexerciseandroid.ui.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dl.dlexerciseandroid.R;
+
+public class ConstraintLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
