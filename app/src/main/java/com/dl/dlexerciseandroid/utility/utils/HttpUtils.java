57c7fcd164691dfef3da43876c6427418344bb7c
Danny Lin <logicmelody@gmail.com>
2018-03-12 08:46:35 +0800

    [Http] Remove some comments

--- app/src/main/java/com/dl/dlexerciseandroid/utility/utils/HttpUtils.java
+++ app/src/main/java/com/dl/dlexerciseandroid/utility/utils/HttpUtils.java
@@ -7,8 +7,6 @@ import android.graphics.BitmapFactory;
 import java.io.IOException;
 import java.util.Map;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
