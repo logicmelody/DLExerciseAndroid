13159d29fe3975257ea9e65e8960cfcea9b8b272
Danny Lin <logicmelody@gmail.com>
2018-03-19 23:03:17 +0800

    [ExoPlayer] Play DASH media

--- app/src/main/java/com/dl/dlexerciseandroid/ui/exoplayer/player/PlayerActivity.java
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/exoplayer/player/PlayerActivity.java
@@ -14,8 +14,15 @@ import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashChunkSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 
 import butterknife.BindView;
@@ -26,6 +33,8 @@ import butterknife.ButterKnife;
  */
 
 public class PlayerActivity extends AppCompatActivity implements PlayerContract.View {
+
+    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
 
     private PlayerContract.Presenter mPresenter;
 
@@ -61,16 +70,23 @@ public class PlayerActivity extends AppCompatActivity implements PlayerContract.
 	protected void onStart() {
 		super.onStart();
 		setupPlayer();
-		prepareMedia();
+		prepareMedia(getString(R.string.exoplayer_url_dash));
 	}
 
 	private void setupPlayer() {
 		hideSystemUi();
 
+        // A factory to create an AdaptiveVideoTrackSelection
+        // 搭配 DefaultBandwidthMeter，可以根據現在的網路狀況和頻寬，來選擇適合的 track 來播放
+        TrackSelection.Factory adaptiveTrackSelectionFactory = new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
+
 		// Roughly a RenderersFactory creates renderers for timestamp synchronized rendering of video, audio and text (subtitles).
 		// The TrackSelector is responsible for selecting from the available audio, video and text tracks
 		// The LoadControl manages buffering of the player.
-		mPlayer = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this), new DefaultTrackSelector(), new DefaultLoadControl());
+		mPlayer = ExoPlayerFactory.newSimpleInstance(
+		        new DefaultRenderersFactory(this),
+                new DefaultTrackSelector(adaptiveTrackSelectionFactory),
+                new DefaultLoadControl());
 
 		mPlayer.setPlayWhenReady(mPlayWhenReady);
 		mPlayer.seekTo(mCurrentWindow, mPlaybackPosition);
@@ -111,16 +127,26 @@ public class PlayerActivity extends AppCompatActivity implements PlayerContract.
 		}
 	}
 
-	private void prepareMedia() {
-		Uri uri = Uri.parse("http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4");
-		MediaSource mediaSource = buildMediaSource(uri);
+	private void prepareMedia(String url) {
+		Uri uri = Uri.parse(url);
+
+//		MediaSource mediaSource = buildExtractorMediaSource(uri);
+        MediaSource mediaSource = buildDashMediaSource(uri);
 
 		mPlayer.prepare(mediaSource, true, false);
 	}
 
-	private MediaSource buildMediaSource(Uri uri) {
+	private MediaSource buildExtractorMediaSource(Uri uri) {
 		return new ExtractorMediaSource.Factory(new DefaultHttpDataSourceFactory(getString(R.string.app_name))).createMediaSource(uri);
 	}
+
+    private MediaSource buildDashMediaSource(Uri uri) {
+        DataSource.Factory manifestDataSourceFactory = new DefaultHttpDataSourceFactory(getString(R.string.app_name));
+        DashChunkSource.Factory dashChunkSourceFactory =
+                new DefaultDashChunkSource.Factory(new DefaultHttpDataSourceFactory(getString(R.string.app_name), BANDWIDTH_METER));
+
+        return new DashMediaSource.Factory(dashChunkSourceFactory, manifestDataSourceFactory).createMediaSource(uri);
+    }
 
 	@Override
     public void setPresenter(PlayerContract.Presenter presenter) {
