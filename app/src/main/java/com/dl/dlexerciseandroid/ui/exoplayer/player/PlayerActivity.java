ee489280ac09a0f86e7986746408451ef11d4933
Danny Lin <danny.l@17.media>
2018-03-19 19:15:18 +0800

    [ExoPlayer] [ExoPlayer] Open PlayerActivity from ExoPlayer main page

--- app/src/main/java/com/dl/dlexerciseandroid/ui/exoplayer/player/PlayerActivity.java
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/exoplayer/player/PlayerActivity.java
@@ -3,7 +3,6 @@ package com.dl.dlexerciseandroid.ui.exoplayer.player;
 import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -46,13 +45,13 @@ public class PlayerActivity extends AppCompatActivity implements PlayerContract.
     public SimpleExoPlayerView mSimpleExoPlayerView;
 
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_player);
-	    ButterKnife.bind(this);
-        initialize();
-    }
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_player);
+		ButterKnife.bind(this);
+		initialize();
+	}
 
 	private void initialize() {
         mPresenter = new PlayerPresenter(this);
