0c456965d6d545eba0922c365335c6712f5d14be
Danny Lin <logicmelody@gmail.com>
2018-04-17 22:56:54 +0800

    [DialogTrigger] Make fullscreen dialog to be fullscreen

--- app/src/main/java/com/dl/dlexerciseandroid/dialog/dialogfragment/fullscreen/FullscreenDialogFragment.java
+++ app/src/main/java/com/dl/dlexerciseandroid/dialog/dialogfragment/fullscreen/FullscreenDialogFragment.java
@@ -1,18 +1,16 @@
 package com.dl.dlexerciseandroid.dialog.dialogfragment.fullscreen;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.dl.dlexerciseandroid.R;
-
-import butterknife.ButterKnife;
-
+// DialogFragment有兩種呈現View的方式：
+// 1. onCreateView(): 這種方式有一個好處，就是可以是情況把這個DialogFragment當成一個Dialog跳出來，或是當成一個一般的Fragment放在UI中，
+//    但是用這種方法有一個缺點，就是沒有onBackPressed()的callback，所以沒有辦法偵測user什麼時候按下back key
+// 2. onCreateDialog(): 這種View的create方式就只能把這個DialogFragment當dialog使用，但是用此方法，
+//                      可以在Dialog中偵測到onBackPressed()的事件
 public class FullscreenDialogFragment extends DialogFragment {
 
     public static final String TAG = FullscreenDialogFragment.class.getName();
@@ -26,20 +24,11 @@ public class FullscreenDialogFragment extends DialogFragment {
         mContext = context;
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fullscreen_dialog, container, false);
-    }
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        FullscreenDialog fullscreenDialog = new FullscreenDialog(mContext);
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        ButterKnife.bind(this, getView());
-        initialize();
-    }
-
-    private void initialize() {
-
+        return fullscreenDialog;
     }
 }
