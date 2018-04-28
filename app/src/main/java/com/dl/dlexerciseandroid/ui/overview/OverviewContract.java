9f711514cb939c31f33b192ee19b94bc1d0b1745
Danny Lin <logicmelody@gmail.com>
2018-04-28 18:25:08 +0800

    [Overview] Check is dev or not in overview page

---
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/overview/OverviewContract.java
@@ -0,0 +1,15 @@
+package com.dl.dlexerciseandroid.ui.overview;
+
+import com.dl.dlexerciseandroid.ui.BasePresenter;
+import com.dl.dlexerciseandroid.ui.BaseView;
+
+public interface OverviewContract {
+
+    interface Presenter extends BasePresenter {
+
+    }
+
+    interface View extends BaseView<OverviewContract.Presenter> {
+
+    }
+}
--- app/src/main/java/com/dl/dlexerciseandroid/ui/overview/OverviewFragment.java
+++ app/src/main/java/com/dl/dlexerciseandroid/ui/overview/OverviewFragment.java
@@ -10,22 +10,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.dl.dlexerciseandroid.BuildConfig;
 import com.dl.dlexerciseandroid.R;
 import com.dl.dlexerciseandroid.model.PersonData;
 import com.google.gson.Gson;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by logicmelody on 2016/3/30.
  */
-public class OverviewFragment extends Fragment {
+public class OverviewFragment extends Fragment implements OverviewContract.View {
 
     // 以後Fragment的tag name都用此class的name來命名比較方便
     // e.g. MusicPlayerFragment
     public static final String TAG = OverviewFragment.class.getName();
 
     private Context mContext;
+    private OverviewContract.Presenter mPresenter;
 
-    private TextView mGtmText;
+    @BindView(R.id.text_view_overview_is_dev)
+    public TextView mTextViewIsDev;
 
 
     @Override
@@ -43,36 +49,22 @@ public class OverviewFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        ButterKnife.bind(this, getView());
         initialize();
     }
 
     private void initialize() {
-        findViews();
-        testGson();
+        mPresenter = new OverviewPresenter(this);
+
+        setupIsDev();
     }
 
-    private void findViews() {
-        mGtmText = (TextView) getView().findViewById(R.id.text_view_overview_gtm_text);
+    private void setupIsDev() {
+        mTextViewIsDev.setText("Is dev = " + BuildConfig.IS_DEV);
     }
 
-    /**
-     * 在使用Gson的時候，要把思維轉換成Json字串跟物件之間的轉換
-     * Serialization:   Object -> Json string
-     * Deserialization: Json string -> Object
-     *
-     * 只要把Java object設定好，基本上就沒有問題了
-     * 這裡的例子是將一串Json的字串轉換成Java object，是個很常見的使用情境
-     */
-    private void testGson() {
-        String jsonString = "{\n" +
-                "  \"age\": \"23\",\n" +
-                "  \"weight\": \"58\",\n" +
-                "  \"height\": \"168\"\n" +
-                "}";
-
-        Gson gson = new Gson();
-        PersonData personData = gson.fromJson(jsonString, PersonData.class);
-
-        Log.d("danny", "testGson() " + personData.toString());
+    @Override
+    public void setPresenter(OverviewContract.Presenter presenter) {
+        mPresenter = presenter;
     }
 }
